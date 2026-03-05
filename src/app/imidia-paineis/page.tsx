3dcf7408ea3c9b6e14e0e359d33fde6d41130a63
jpguizelini <ja1dev@proton.me>
2026-03-05 01:02:14 -0300

    props gradienttext finalizada

--- src/app/imidia-paineis/page.tsx
+++ src/app/imidia-paineis/page.tsx
@@ -8,7 +8,7 @@ export default function ImidiaPaineis() {
     <div className="bg-[url('/images/bg.jpg')] bg-no-repeat bg-center-top bg-cover min-h-screen">
         <ParallaxBanner imageSrc="/images/outdoor/bras-olaia.png" imageAlt="outdoorbrasolaia" />
         <div className="bg-transparent px-4 sm:px-8 md:px-16 lg:px-[60px] py-16">
-          <GradientText from="#c3d33f" via="#079c9e" to="#c3d33f" className="mb-0">
+          <GradientText size="text-[65px] sm:text-[80px] md:text-[65px]" className="mb-0">
             a maior em número de cidades no interior <br></br>
             e a melhor em versatilidades de ativos.
           </GradientText>
--- src/components/ui/GradientText.tsx
+++ src/components/ui/GradientText.tsx
@@ -6,6 +6,8 @@ interface GradientTextProps {
   via?: string;
   to?: string;
   className?: string;
+  text?: string;
+  size?: string;
 }
 
 export default function GradientText({
@@ -14,10 +16,11 @@ export default function GradientText({
   via = '#079c9e',
   to = '#c3d33f',
   className = '',
+  size = 'text-[28px] sm:text-[35px] md:text-[47px]',
 }: GradientTextProps) {
   return (
     <motion.p
-        className="font-bebas font-regular text-[28px] sm:text-[35px] md:text-[47px] leading-tight text-center break-words px-2"
+        className={`font-bebas font-regular ${size} leading-tight text-center break-words px-2`}
         initial={{ opacity: 0, scale: 0.9 }}
         whileInView={{ opacity: 1, scale: 1 }}
         viewport={{ once: true, amount: 0.3 }}
